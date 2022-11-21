b98aa0f147928305d34c37d950229ff537a2a002
Joel Mejia <nahomygmzb@gmail.com>
2022-11-21 13:07:11 -0400

    commit #1

--- Ventas/Program.cs
+++ Ventas/Program.cs
@@ -1,10 +1,22 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
 using Ventas.Models;
 
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<VENTASContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbContext")));
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins, builder =>
+    {
+        builder.WithOrigins("https://localhost:7197").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +29,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(options =>
+{
+    options.WithOrigins("http://localhost:3000");
+    options.WithOrigins("http://localhost:4200");
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
